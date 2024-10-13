dcf72670c5bbbd9dd0c68b9227aa1c4d5c150529
THETASOLA <tao.solan@gmail.com>
2024-10-13 15:21:04 +0200

    git init

---
+++ VM/Lille/dhcp/dhcp_config.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y isc-dhcp-server
+
+# Configure
+cat << EOF > /etc/dhcp/dhcpd.conf
+default-lease-time 600;
+max-lease-time 7200;
+
+subnet 192.168.20.0 netmask 255.255.255.0 {
+}
+
+subnet 192.168.32.0 netmask 255.255.240.0 {
+  range 192.168.32.100 192.168.47.254;
+  option routers 192.168.32.1;
+  option domain-name-servers 192.168.12.2;
+  option domain-name "lille.local";
+}
+
+EOF
+
+sed -i 's/INTERFACESv4=""/INTERFACESv4="eth1"/' /etc/default/isc-dhcp-server
+
+# Restart
+systemctl restart isc-dhcp-server
---
+++ VM/Lille/firewall_ext/firewall_config.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Flush existing rules
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+
+# Set default policies
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+# Allow loopback
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+# Allow established and related connections
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# Allow SSH (adjust if your SSH port is different)
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+# Allow VPN (OpenVPN)
+iptables -A INPUT -p udp --dport 1194 -j ACCEPT
+
+# Allow DNS
+iptables -A INPUT -p udp --dport 53 -j ACCEPT
+iptables -A INPUT -p tcp --dport 53 -j ACCEPT
+
+# Allow HTTP and HTTPS
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+# Allow ICMP (ping)
+iptables -A INPUT -p icmp -j ACCEPT
+
+# NAT (assuming eth0 is WAN and eth1 is LAN)
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+# Forward VPN traffic to VPN server
+iptables -A FORWARD -i eth0 -o eth1 -p udp --dport 1194 -d 192.168.10.2 -j ACCEPT
+
+# Allow forwarding for established connections
+iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# Save rules
+iptables-save > /etc/iptables/rules.v4
+
+# Install iptables-persistent to load rules on boot
+apt-get update
+apt-get install -y iptables-persistent
